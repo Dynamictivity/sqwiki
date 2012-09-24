21d3f1effff0fed572cea3267329451c15aa722d
Theaxiom <travis.rowland@gmail.com>
2012-09-23 22:42:49 -0700

    Basic jQueryUI Implementation

---
+++ webroot/js/sqwiki.js
@@ -0,0 +1,8 @@
+$(function() {
+
+	$('#header').addClass('ui-widget ui-widget-header ui-state-active ui-helper-reset ui-helper-clearfix');
+	$('#content').addClass('ui-widget ui-widget-content ui-helper-reset ui-helper-clearfix');
+	$('#footer').addClass('ui-widget ui-widget-header ui-helper-reset ui-helper-clearfix');
+	$('form input, form textarea').addClass('ui-widget ui-corner-all');
+
+}); 
