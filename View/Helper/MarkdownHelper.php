63e49289a193af737c19b281398036e4d07087df
Theaxiom <travis.rowland@gmail.com>
2012-09-29 17:03:42 -0700

    formatting wiki output for sources

--- View/Helper/MarkdownHelper.php
+++ View/Helper/MarkdownHelper.php
@@ -6,7 +6,13 @@ class MarkdownHelper extends AppHelper {
 	//public $helpers = array('Html');
 
 	public function parse($markup) {
-		return Markdown($markup);
+		return $this->__formatForWiki(Markdown($markup));
+	}
+
+	private function __formatForWiki($output) {
+		$output = str_replace(array('</h1>', '</h2>', '</h3>', '</h4>', '</h5>', '</h6>', '[[Sources]]'), array('</h1><hr />', '</h2><hr />', '</h3><hr />', '</h4><hr />', '</h5><hr />', '</h6><hr />', '<a name="sources">Sources:</a>'), $output);
+		$output = str_replace('href="#sources"', 'href="#sources" class="sources"', $output);
+		return $output;
 	}
 
 }
