7bebae6058ad6a7dde1674ae27821f9a3338fbb4
Theaxiom <travis.rowland@gmail.com>
2012-09-29 17:11:55 -0700

    source symantecs

--- View/Helper/MarkdownHelper.php
+++ View/Helper/MarkdownHelper.php
@@ -10,8 +10,8 @@ class MarkdownHelper extends AppHelper {
 	}
 
 	private function __formatForWiki($output) {
-		$output = str_replace(array('</h1>', '</h2>', '</h3>', '</h4>', '</h5>', '</h6>', '[[Sources]]'), array('</h1><hr />', '</h2><hr />', '</h3><hr />', '</h4><hr />', '</h5><hr />', '</h6><hr />', '<a name="sources">Sources:</a>'), $output);
-		$output = str_replace('href="#sources"', 'href="#sources" class="sources"', $output);
+		$output = str_replace(array('</h1>', '</h2>', '</h3>', '</h4>', '</h5>', '</h6>', '[[Sources]]'), array('</h1><hr />', '</h2><hr />', '</h3><hr />', '</h4><hr />', '</h5><hr />', '</h6><hr />', '<a name="source">Sources:</a>'), $output);
+		$output = str_replace('href="#source"', 'href="#source" class="source"', $output);
 		return $output;
 	}
 
