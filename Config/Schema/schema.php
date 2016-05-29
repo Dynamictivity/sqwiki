1a9339ed2d3e2a6c6f9dc47c3048cfe44e314533
Travis Rowland <travis.rowland@dynamictivity.com>
2016-05-29 04:57:52 -0700

    Setup database migrations FIXES #15

--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -12,27 +12,15 @@ class AppSchema extends CakeSchema
     {
     }
 
-    public $achievements = array(
-        'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-        'name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-        'user_field' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-        'user_field_count' => array('type' => 'integer', 'null' => false, 'default' => null),
-        'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
-        'updated' => array('type' => 'datetime', 'null' => false, 'default' => null),
-        'indexes' => array(
-            'PRIMARY' => array('column' => 'id', 'unique' => 1)
-        ),
-        'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
-    );
     public $article_revisions = array(
-        'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-        'article_id' => array('type' => 'integer', 'null' => false, 'default' => null),
-        'user_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+        'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+        'article_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
+        'user_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
         'ip_address' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
         'summary' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
         'content' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
         'is_active' => array('type' => 'boolean', 'null' => false, 'default' => '1'),
-        'reviewed_by_user_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+        'reviewed_by_user_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
         'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
         'updated' => array('type' => 'datetime', 'null' => true, 'default' => null),
         'indexes' => array(
@@ -40,14 +28,15 @@ class AppSchema extends CakeSchema
         ),
         'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
     );
+
     public $articles = array(
-        'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+        'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
         'title' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
         'slug' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-        'user_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+        'user_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
         'ip_address' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-        'article_revision_count' => array('type' => 'integer', 'null' => true, 'default' => null),
-        'comment_count' => array('type' => 'integer', 'null' => true, 'default' => null),
+        'article_revision_count' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
+        'comment_count' => array('type' => 'integer', 'null' => true, 'default' => null, 'unsigned' => false),
         'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
         'updated' => array('type' => 'datetime', 'null' => true, 'default' => null),
         'indexes' => array(
@@ -55,11 +44,12 @@ class AppSchema extends CakeSchema
         ),
         'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
     );
+
     public $comments = array(
-        'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-        'user_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+        'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
+        'user_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
         'ip_address' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-        'article_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+        'article_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
         'comment' => array('type' => 'text', 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
         'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
         'indexes' => array(
@@ -67,16 +57,17 @@ class AppSchema extends CakeSchema
         ),
         'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
     );
+
     public $users = array(
-        'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+        'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
         'username' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
         'email' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
         'password' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
-        'role_id' => array('type' => 'integer', 'null' => false, 'default' => null),
+        'role_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false),
         'token' => array('type' => 'string', 'null' => true, 'default' => null, 'length' => 36, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
-        'article_count' => array('type' => 'integer', 'null' => false, 'default' => '0'),
-        'article_revision_count' => array('type' => 'integer', 'null' => false, 'default' => '0'),
-        'comment_count' => array('type' => 'integer', 'null' => false, 'default' => '0'),
+        'article_count' => array('type' => 'integer', 'null' => false, 'default' => '0', 'unsigned' => false),
+        'article_revision_count' => array('type' => 'integer', 'null' => false, 'default' => '0', 'unsigned' => false),
+        'comment_count' => array('type' => 'integer', 'null' => false, 'default' => '0', 'unsigned' => false),
         'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
         'updated' => array('type' => 'datetime', 'null' => true, 'default' => null),
         'indexes' => array(
@@ -84,4 +75,5 @@ class AppSchema extends CakeSchema
         ),
         'tableParameters' => array('charset' => 'latin1', 'collate' => 'latin1_swedish_ci', 'engine' => 'InnoDB')
     );
+
 }
